fc95d1c26ffe4a1a64b18fcbac0794b863d60512
murimolda <35343729+murimolda@users.noreply.github.com>
2022-02-02 17:55:41 +0300

    feat: add morse-decoder function

--- src/index.js
+++ src/index.js
@@ -38,7 +38,39 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+       let letter = '';
+    let simbol = '';
+    let result = '';
+    let space = ' ';
+
+    for (let i = 0; i < expr.length; i += 10) {
+        letter = expr.slice(i, i + 10);
+        if (letter === '**********') {
+            space = ' ';
+            result = `${result}${space}`
+        } else {
+            while (simbol.length < 5) {
+                for (let j = letter.length; j > 1; j -= 2) {
+                    let a = `${letter[j - 2]}${letter[j - 1]}`;
+                    if (a === '10') {
+                        simbol = `.${simbol}`
+                    } else if (a === '11') {
+                        simbol = `-${simbol}`
+                    } else if (a === '00') {
+                        simbol = ` ${simbol}`
+                    }
+                }
+            }
+            for (const [key, value] of Object.entries(MORSE_TABLE)) {
+                if (`${key}` === simbol.trim()) {
+                    result = `${result}${value}`
+                }
+            }
+            simbol = '';
+        }
+    }
+    return result;
+
 }
 
 module.exports = {
